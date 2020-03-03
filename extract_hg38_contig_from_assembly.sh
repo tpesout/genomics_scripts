c6ad06abc10e13e742d3db21a7a25e8dacf2ac8d
Ubuntu <ubuntu@ip-172-31-24-11.us-west-2.compute.internal>
2020-03-03 18:04:51 +0000

    adding hg38 extraction script

---
+++ extract_hg38_contig_from_assembly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ASM=$1
+export TC=10
+export CHR=chrX
+export HG38=/home/ubuntu/data/human/reference/hg38/hg38.fa
+
+minimap2 -ax asm20 -t $TC $HG38 $ASM | samtools view -hb >$ASM.unsorted.bam
+samtools sort -@ $TC $ASM.unsorted.bam | samtools view -hb >$ASM.bam
+samtools index -@ $TC $ASM.bam
+rm $ASM.unsorted.bam
+
+samtools view -F 0x904 $ASM.bam $CHR | awk '{print $1}' | uniq | sort | uniq >$ASM.hg38_${CHR}_0x904_segments.txt
+samtools view -F 0x904 -q 60 $ASM.bam $CHR | awk '{print $1}' | uniq | sort | uniq >$ASM.hg38_${CHR}_0x904q60_segments.txt
+
+extract_fasta_segments.py -i $ASM -s $ASM.hg38_${CHR}_0x904_segments.txt -o $ASM.hg38_${CHR}_0x904.fa
+extract_fasta_segments.py -i $ASM -s $ASM.hg38_${CHR}_0x904q60_segments.txt -o $ASM.hg38_${CHR}_0x904q60.fa
