c7773a9d37e2bc8b32f56e46f6257b351e5e690a
Trevor Pesout <tpesout@ucsc.edu>
2021-05-28 13:48:17 -0700

    updating stratified happy results

--- plot_stratified_happy_results.py
+++ plot_stratified_happy_results.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # plt.style.use('ggplot')
-# text_fontsize = 8
+text_fontsize = 8
 # # plt.rcParams['ytick.labelsize']=text_fontsize+4
 # plt.rcParams['pdf.fonttype'] = 42
 # plt.switch_backend('agg')
@@ -11,68 +11,70 @@ import matplotlib.pyplot as plt
 SNP_INDEX=2
 INDEL_INDEX=0
 
+REGION="Subset"
+SAMPLE="Sample"
+TYPE="Type"
+RECALL="METRIC.Recall"
+PRECISION="METRIC.Precision"
+F1="METRIC.F1_Score"
+
 # read inputs / CSV from hap.py
+DATA = pd.read_csv("/home/tpesout/work/nicu/stratification_analysis/HG002_barcode_nobc_stratifications.csv")
 
-HG003_CCS_DV = pd.read_csv("./figure_inputs/Figure_2bc_difficult_regions/HG003_DV_v110_DV_margin_DV.extended.csv")
-HG004_CCS_DV = pd.read_csv("./figure_inputs/Figure_2bc_difficult_regions/HG004_DV_v110_DV_margin_DV.extended.csv")
-HG003_ILM_DV = pd.read_csv("./figure_inputs/Figure_2bc_difficult_regions/HG003_illumina_deepvariant_v1.extended.csv")
-HG004_ILM_DV = pd.read_csv("./figure_inputs/Figure_2bc_difficult_regions/HG004_illumina_deepvariant_v1.extended.csv")
-HG003_ONT_DV = pd.read_csv("./figure_inputs/Figure_2bc_difficult_regions/HG003_guppy422_90x_pmd_giab_421.extended.csv")
-HG004_ONT_DV = pd.read_csv("./figure_inputs/Figure_2bc_difficult_regions/HG004_guppy422_90x_pmd_giab_421.extended.csv")
+SAMPLES = {"HG002 Barcoded": "HG002_BC04", "HG002 Non-Barcoded": "HG002_No_BC"}
+SAMPLE_KEYS = ["HG002 Barcoded", "HG002 Non-Barcoded"]
+REGIONS = {
+    "Homopolymer": "GRCh37_AllHomopolymers_gt6bp_imperfectgt10bp_slop5.bed.gz",
+    "Non-Homopolymer": "GRCh37_notinAllHomopolymers_gt6bp_imperfectgt10bp_slop5.bed.gz",
+    "Difficult Regions": "GRCh37_alldifficultregions.bed.gz",
+    "Non-Difficult Regions": "GRCh37_notinalldifficultregions.bed.gz",
+    "All Regions": "*",
+    "Exons": "all_exons"
+}
+REGION_KEYS = ["All Regions", "Homopolymer", "Non-Homopolymer", "Difficult Regions", "Non-Difficult Regions", "Exons"]
+TYPE_KEYS = ["SNP","INDEL"]
 
-Regions = ["Major Histocompatibility Complex (MHC)", "Segmental Duplications", "Low mappability regions", "250bp+ non-unique regions"]
-Region_index = [2752 - 2, 2854 - 2, 2826 - 2, 2830 - 2]
 x_lims = [0.9, 0.9, 0.9, 0.5]
 x_tick_frq = [0.02, 0.02, 0.02, 0.1]
+x_lims = [0.6 for _ in REGION_KEYS]
+x_tick_frq = [0.1 for _ in REGION_KEYS]
 
-fig, axes = plt.subplots(nrows=1, ncols=len(Regions), figsize=(8, 3.5))
+fig, axes = plt.subplots(nrows=2, ncols=len(REGION_KEYS), figsize=(2*len(REGION_KEYS), 7))
 
-for i in range(len(Regions)):
-    print("PROCESSING: ", HG003_ONT_DV['Subset'][Region_index[i]])
-    # HG003 RPF HG004 RPF
-    HG003_SCORES = [HG003_ONT_DV['METRIC.Recall'][Region_index[i]],
-                    HG003_ONT_DV['METRIC.Precision'][Region_index[i]],
-                    HG003_ONT_DV['METRIC.F1_Score'][Region_index[i]],
-                    HG003_ILM_DV['METRIC.Recall'][Region_index[i]],
-                    HG003_ILM_DV['METRIC.Precision'][Region_index[i]],
-                    HG003_ILM_DV['METRIC.F1_Score'][Region_index[i]],
-                    HG003_CCS_DV['METRIC.Recall'][Region_index[i]],
-                    HG003_CCS_DV['METRIC.Precision'][Region_index[i]],
-                    HG003_CCS_DV['METRIC.F1_Score'][Region_index[i]]]
-
-    HG004_SCORES = [HG004_ONT_DV['METRIC.Recall'][Region_index[i]],
-                    HG004_ONT_DV['METRIC.Precision'][Region_index[i]],
-                    HG004_ONT_DV['METRIC.F1_Score'][Region_index[i]],
-                    HG004_ILM_DV['METRIC.Recall'][Region_index[i]],
-                    HG004_ILM_DV['METRIC.Precision'][Region_index[i]],
-                    HG004_ILM_DV['METRIC.F1_Score'][Region_index[i]],
-                    HG004_CCS_DV['METRIC.Recall'][Region_index[i]],
-                    HG004_CCS_DV['METRIC.Precision'][Region_index[i]],
-                    HG004_CCS_DV['METRIC.F1_Score'][Region_index[i]]]
-
-    Headers = ['Region', 'Sample', 'Sequencing technology', 'Total truth', 'True positives', 'False negatives', 'False positives', 'Recall', 'Precision', 'F1-score']
-    print(','.join(Headers))
-    print(','.join([Regions[i], 'HG003', 'ONT', str(HG003_ONT_DV['TRUTH.TOTAL'][Region_index[i]]), str(HG003_ONT_DV['TRUTH.TP'][Region_index[i]]), str(HG003_ONT_DV['TRUTH.FN'][Region_index[i]]), str(HG003_ONT_DV['QUERY.FP'][Region_index[i]]), str(HG003_ONT_DV['METRIC.Recall'][Region_index[i]]), str(HG003_ONT_DV['METRIC.Precision'][Region_index[i]]), str(HG003_ONT_DV['METRIC.F1_Score'][Region_index[i]])]))
-    print(','.join([Regions[i], 'HG003', 'Illumina',        str(HG003_ILM_DV['TRUTH.TOTAL'][Region_index[i]]), str(HG003_ILM_DV['TRUTH.TP'][Region_index[i]]), str(HG003_ILM_DV['TRUTH.FN'][Region_index[i]]), str(HG003_ILM_DV['QUERY.FP'][Region_index[i]]), str(HG003_ILM_DV['METRIC.Recall'][Region_index[i]]), str(HG003_ILM_DV['METRIC.Precision'][Region_index[i]]), str(HG003_ILM_DV['METRIC.F1_Score'][Region_index[i]])]))
-    print(','.join([Regions[i], 'HG003', 'PacBio',     str(HG003_CCS_DV['TRUTH.TOTAL'][Region_index[i]]), str(HG003_CCS_DV['TRUTH.TP'][Region_index[i]]), str(HG003_CCS_DV['TRUTH.FN'][Region_index[i]]), str(HG003_CCS_DV['QUERY.FP'][Region_index[i]]), str(HG003_CCS_DV['METRIC.Recall'][Region_index[i]]), str(HG003_CCS_DV['METRIC.Precision'][Region_index[i]]), str(HG003_CCS_DV['METRIC.F1_Score'][Region_index[i]])]))
-    print(','.join([Regions[i], 'HG004', 'ONT', str(HG004_ONT_DV['TRUTH.TOTAL'][Region_index[i]]), str(HG004_ONT_DV['TRUTH.TP'][Region_index[i]]), str(HG004_ONT_DV['TRUTH.FN'][Region_index[i]]), str(HG004_ONT_DV['QUERY.FP'][Region_index[i]]), str(HG004_ONT_DV['METRIC.Recall'][Region_index[i]]), str(HG004_ONT_DV['METRIC.Precision'][Region_index[i]]), str(HG004_ONT_DV['METRIC.F1_Score'][Region_index[i]])]))
-    print(','.join([Regions[i], 'HG004', 'Illumina',        str(HG004_ILM_DV['TRUTH.TOTAL'][Region_index[i]]), str(HG004_ILM_DV['TRUTH.TP'][Region_index[i]]), str(HG004_ILM_DV['TRUTH.FN'][Region_index[i]]), str(HG004_ILM_DV['QUERY.FP'][Region_index[i]]), str(HG004_ILM_DV['METRIC.Recall'][Region_index[i]]), str(HG004_ILM_DV['METRIC.Precision'][Region_index[i]]), str(HG004_ILM_DV['METRIC.F1_Score'][Region_index[i]])]))
-    print(','.join([Regions[i], 'HG004', 'PacBio',     str(HG004_CCS_DV['TRUTH.TOTAL'][Region_index[i]]), str(HG004_CCS_DV['TRUTH.TP'][Region_index[i]]), str(HG004_CCS_DV['TRUTH.FN'][Region_index[i]]), str(HG004_CCS_DV['QUERY.FP'][Region_index[i]]), str(HG004_CCS_DV['METRIC.Recall'][Region_index[i]]), str(HG004_CCS_DV['METRIC.Precision'][Region_index[i]]), str(HG004_CCS_DV['METRIC.F1_Score'][Region_index[i]])]))
+for i in range(len(REGION_KEYS)):
+    SNP_SCORES = [
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[0]]][DATA[TYPE]==TYPE_KEYS[0]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][RECALL].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[0]]][DATA[TYPE]==TYPE_KEYS[0]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][PRECISION].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[0]]][DATA[TYPE]==TYPE_KEYS[0]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][F1].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[1]]][DATA[TYPE]==TYPE_KEYS[0]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][RECALL].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[1]]][DATA[TYPE]==TYPE_KEYS[0]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][PRECISION].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[1]]][DATA[TYPE]==TYPE_KEYS[0]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][F1].values[0],
+    ]
+    INDEL_SCORES = [
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[0]]][DATA[TYPE]==TYPE_KEYS[1]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][RECALL].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[0]]][DATA[TYPE]==TYPE_KEYS[1]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][PRECISION].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[0]]][DATA[TYPE]==TYPE_KEYS[1]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][F1].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[1]]][DATA[TYPE]==TYPE_KEYS[1]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][RECALL].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[1]]][DATA[TYPE]==TYPE_KEYS[1]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][PRECISION].values[0],
+        DATA[DATA[SAMPLE]==SAMPLES[SAMPLE_KEYS[1]]][DATA[TYPE]==TYPE_KEYS[1]][DATA[REGION]==REGIONS[REGION_KEYS[i]]][F1].values[0],
+    ]
 
     # Set colors
-    ONT_COLOR = [(238/256.0, 39/256.0, 8/256.0, 0.7), (238/256.0, 39/256.0, 8/256.0, 0.8), (238/256.0, 39/256.0, 8/256.0, 1.0)]
-    ILM_COLOR = [(238/256.0, 124/256.0, 8/256.0, 0.7), (238/256.0, 124/256.0, 8/256.0, 0.8), (238/256.0, 124/256.0, 8/256.0, 1.0)]
-    CCS_COLOR = [(10/256.0, 118/256.0, 148/256.0, 0.7), (10/256.0, 118/256.0, 148/256.0, 0.8), (10/256.0, 118/256.0, 148/256.0, 1.0)]
+    BARCODE_COLOR = [(238/256.0, 124/256.0, 8/256.0, 0.7), (238/256.0, 124/256.0, 8/256.0, 0.8), (238/256.0, 124/256.0, 8/256.0, 1.0)]
+    NON_BARCODE_COLOR = [(10/256.0, 118/256.0, 148/256.0, 0.7), (10/256.0, 118/256.0, 148/256.0, 0.8), (10/256.0, 118/256.0, 148/256.0, 1.0)]
 
-    HG003_COLOR = ONT_COLOR + ILM_COLOR + CCS_COLOR
-    HG004_COLOR = ONT_COLOR + ILM_COLOR + CCS_COLOR
+    SNP_COLORS = BARCODE_COLOR + NON_BARCODE_COLOR
+    INDEL_COLORS = BARCODE_COLOR + NON_BARCODE_COLOR
 
     # Bar charts
     N = 4
     ind = np.arange(N)
     width = 0.98
-    bar_positions2 = [0, 1, 2, 3.25, 4.25, 5.25, 6.50, 7.50, 8.50]
-    bar_positions1 = [10.50, 11.50, 12.50, 13.75, 14.75, 15.75, 17, 18, 19]
+
+    bar_positions2 = [0, 1, 2, 3.25, 4.25, 5.25]
+    bar_positions1 = [7.25, 8.25, 9.25, 10.5, 11.5, 12.5]
+    # bar_positions2 = [0, 1, 2, 3.25, 4.25, 5.25, 6.50, 7.50, 8.50]
+    # bar_positions1 = [10.50, 11.50, 12.50, 13.75, 14.75, 15.75, 17, 18, 19]
 
     bar_positions1.reverse()
     bar_positions2.reverse()
@@ -85,9 +87,9 @@ for i in range(len(Regions)):
     x_lim_min = x_lims[i]
     x_lim_max = 1.00
     first_start = 0
-    p1 = axes[i].barh(bar_positions1, HG003_SCORES, width, color = HG003_COLOR)
-    axes[i].hlines(9.50, x_lim_min, x_lim_max, colors='black', linestyles='solid')
-    p2 = axes[i].barh(bar_positions2, HG004_SCORES, width, color = HG004_COLOR)
+    p1 = axes[i].barh(bar_positions1, SNP_SCORES, width, color = SNP_COLORS)
+    axes[i].hlines((bar_positions1[-1] + bar_positions2[0]) / 2, x_lim_min, x_lim_max, colors='black', linestyles='solid')
+    p2 = axes[i].barh(bar_positions2, INDEL_SCORES, width, color = INDEL_COLORS)
 
 
 
@@ -98,7 +100,7 @@ for i in range(len(Regions)):
 
 
     value_offset = (x_lim_max - x_lim_min) / 10
-    for ii, v in enumerate(HG003_SCORES):
+    for ii, v in enumerate(SNP_SCORES):
         # axes[i].text(text_start, bar_positions1[ii], Category[ii % 3], va='center', color='black', fontsize=text_fontsize)
         if v < 0.91 and x_lim_min == 0.9 or v < 0.6:
             loc = v
@@ -114,7 +116,7 @@ for i in range(len(Regions)):
             color = 'white'
 
         axes[i].text(v, bar_positions1[ii], str(v)[0:5], va=va_align, ha=ha_align, color=color, fontsize=text_fontsize)
-    for ii, v in enumerate(HG004_SCORES):
+    for ii, v in enumerate(INDEL_SCORES):
         # axes[i].text(text_start, bar_positions2[ii], Category[ii % 3], va='center', color='black', fontsize=text_fontsize)
         if v < 0.91 and x_lim_min == 0.9 or v < 0.6:
             loc = v
@@ -130,17 +132,16 @@ for i in range(len(Regions)):
             color = 'white'
         axes[i].text(v, bar_positions2[ii], str(v)[0:5], va=va_align, ha=ha_align, color=color, fontsize=text_fontsize)
 
-    if i == len(Regions) - 1:
-        caller_name_offser = x_lim_max + (0.08 * (1-x_lim_max)) # -0.13 for 0.0
-        sample_name_offset = x_lim_max + (0.10 * (1-x_lim_max)) # -0.07 for 0.0
-        samples = ["HG003 (SNP)", "HG004 (SNP)"]
-        axes[i].text(sample_name_offset, bar_positions1[0] + ((bar_positions1[-1] - bar_positions1[0]) / 2), samples[0], va='center', fontsize=text_fontsize, rotation=270)
-        axes[i].text(sample_name_offset, bar_positions2[0] + ((bar_positions2[-1] - bar_positions2[0]) / 2), samples[1], va='center', fontsize=text_fontsize, rotation=270)
+    if i == len(REGIONS) - 1:
+        # caller_name_offser = x_lim_max + (0.08 * (1-x_lim_max)) # -0.13 for 0.0
+        type_name_offset = x_lim_max + (0.10 * (1-x_lim_max)) # -0.07 for 0.0
+        axes[i].text(type_name_offset, bar_positions1[0] + ((bar_positions1[-1] - bar_positions1[0]) / 2), TYPE_KEYS[0], va='center', fontsize=text_fontsize, rotation=270)
+        axes[i].text(type_name_offset, bar_positions2[0] + ((bar_positions2[-1] - bar_positions2[0]) / 2), TYPE_KEYS[1], va='center', fontsize=text_fontsize, rotation=270)
 
     if i == 0:
-        categories = ["Nanopore", "Illumina", "PacBio\nHiFi"]
-        mid_points = [bar_positions1[1], bar_positions1[4], bar_positions1[7],
-                      bar_positions2[1], bar_positions2[4], bar_positions2[7]]
+        categories = SAMPLE_KEYS
+        mid_points = [bar_positions1[1], bar_positions1[4],
+                      bar_positions2[1], bar_positions2[4]]
         all_categories = categories + categories
         axes[i].set_yticks(mid_points)
         axes[i].set_yticklabels(all_categories, fontsize=text_fontsize+1, color='black')
@@ -159,13 +160,13 @@ for i in range(len(Regions)):
     if i == 0:
         caller_name_offset = x_lim_min - (0.08 * (1-x_lim_max)) # -0.13 for 0.0
 
-        for ii, v in enumerate(HG003_SCORES):
+        for ii, v in enumerate(SNP_SCORES):
             axes[i].text(x_lim_min + 0.001, bar_positions2[ii], categories[ii % 3], color='white', va='center', ha='left', fontsize=text_fontsize)
             axes[i].text(x_lim_min + 0.001, bar_positions1[ii], categories[ii % 3], color='white', va='center', ha='left', fontsize=text_fontsize)
 
     axes[i].tick_params(axis=u'both', which=u'both',length=0)
     axes[i].set_xlim(x_lim_min, x_lim_max)
-    axes[i].set_title(Regions[i], fontsize=text_fontsize + 1)
+    axes[i].set_title(REGION_KEYS[i], fontsize=text_fontsize + 1)
 
     x_ticks = np.arange(x_lims[i] + x_tick_frq[i], 1.01, x_tick_frq[i] * 2)
     x_lables = ["%.2lf" % round(x,2) for x in x_ticks]
